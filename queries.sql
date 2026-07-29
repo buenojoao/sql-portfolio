7359632b0a3d23803c175584544ce29065e291f2
João Victor Bueno <joao220904@gmail.com>
2026-07-29 16:14:50 -0300

    feat: altera filtro para clientes pendentes

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Consultas SQL - Treino e Estudos
-SELECT * FROM clientes WHERE status = 'ativo';
+SELECT * FROM clientes WHERE status = 'PENDENTE';
 -- Segunda consulta
 SELECT * FROM pedidos WHERE data > '2026-01-01';
 -- Consulta 3: Vendas acima de 1000 reais
