603796f049ef79e270b35bb25005c1ab7f38177d
João Victor Bueno <joao220904@gmail.com>
2026-07-29 15:48:04 -0300

    feat: adiciona consulta de pedidos e relatorio de vendas

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 -- Consultas SQL - Treino e Estudos
 SELECT * FROM clientes WHERE status = 'ativo';
 -- Segunda consulta
+SELECT * FROM pedidos WHERE data > '2026-01-01';
+-- Consulta 3: Vendas acima de 1000 reais
+SELECT * FROM vendas WHERE valor > 1000;
